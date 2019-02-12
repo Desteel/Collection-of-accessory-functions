82a9c1c953e92dea629560d17a305aae675e20df
Desteel <alexgowargame@yandex.ru>
2019-02-12 16:10:44 +0400

    add service.js

---
+++ service.js
@@ -0,0 +1,41 @@
+let getMaxNumber = $numberArray => {
+    var max = 0;
+
+    for (var number of $numberArray) {
+        if (number > max) {
+            max = number;
+        }
+    }
+
+    return max;
+}
+
+let getTag = $elem => $elem[0].nodeName.toLowerCase();
+
+let getContent = $target => {
+    var value,
+        tag = getTag($target);
+
+    if (tag === "input") {
+        value = $target.val();
+    } else if (tag === "img") {
+        value = $target.attr("src");
+    } else if (tag === "div" || tag === "span" || tag === "label" || tag === "td") {
+        value = $target.text();
+    }
+    return value;
+}
+
+let callbackInit = $callback => {
+    if ($callback && typeof($callback) === "function") {
+        $callback();
+    }
+}
+
+let eventInit = ($event, $elem, $function) => {
+    $elem.on($event, function() {
+        callbackInit($function);
+    });
+}
+
+export { getMaxNumber, getContent, callbackInit, eventInit };
